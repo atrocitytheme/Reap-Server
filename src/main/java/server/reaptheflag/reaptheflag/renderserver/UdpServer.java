04b235c5685295b42c65780d151796c8d8d96e91
Junxuan Wu <atrocitytheme@gmail.com>
2019-07-15 11:31:58 +0800

    fix message recieve bugs

--- src/main/java/server/reaptheflag/reaptheflag/renderserver/UdpServer.java
+++ src/main/java/server/reaptheflag/reaptheflag/renderserver/UdpServer.java
@@ -8,6 +8,10 @@ import io.netty.channel.ChannelOption;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioDatagramChannel;
+import io.netty.handler.codec.DatagramPacketDecoder;
+import io.netty.handler.codec.protobuf.ProtobufDecoder;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
 import server.reaptheflag.reaptheflag.renderserver.configuration.PacketHandler;
 
 import java.net.InetAddress;
@@ -27,14 +31,14 @@ public class UdpServer {
 
                     @Override
                     protected void initChannel(NioDatagramChannel nioDatagramChannel) throws Exception {
+                        System.out.println("initialized!");
                         ChannelPipeline pipe = nioDatagramChannel.pipeline();
                         pipe.addLast(new PacketHandler());
                     }
-                });
+                }); // packet handler is the bootstrap of the processing program
 
         InetAddress address = InetAddress.getLocalHost();
         System.out.println("udp server initialized!" + address);
         programBootStrap.bind(address, port).sync().channel().closeFuture().await();
-        System.out.println("passed!");
     }
 }
--- src/main/java/server/reaptheflag/reaptheflag/renderserver/configuration/PacketHandler.java
+++ src/main/java/server/reaptheflag/reaptheflag/renderserver/configuration/PacketHandler.java
@@ -1,22 +1,22 @@
 package server.reaptheflag.reaptheflag.renderserver.configuration;
-
+/***
+ * bootstrap event handler
+ * components: eventTrigger layer, dispatcher layer, eventhandler lauyer, validation layer
+ */
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.socket.DatagramPacket;
 
-import java.net.DatagramPacket;
-import java.net.InetAddress;
 
 public class PacketHandler extends SimpleChannelInboundHandler<DatagramPacket> {
 
     @Override
     protected void channelRead0(ChannelHandlerContext channelHandlerContext, DatagramPacket datagramPacket) throws Exception {
-        final InetAddress addr = datagramPacket.getAddress();
-        final ByteBuf buf = Unpooled.wrappedBuffer(datagramPacket.getData());
+        final ByteBuf buf = Unpooled.wrappedBuffer(datagramPacket.content());
         final int length = buf.readableBytes();
         final byte[] receiveBuf = new byte[length];
         buf.readBytes(receiveBuf);
-        System.out.println("Systen handling...");
     }
 }
