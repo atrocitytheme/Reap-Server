f9760d1418914f1682465135b65a6bc41af24e37
Junxuan Wu <atrocitytheme@gmail.com>
2019-07-15 15:34:24 +0800

    prepare batch process of dispatch layer

---
+++ src/main/java/server/reaptheflag/reaptheflag/renderserver/dispatcher/DispatcherPostBeanProcessor.java
@@ -0,0 +1,7 @@
+package server.reaptheflag.reaptheflag.renderserver.dispatcher;
+/**
+ * configure the annotation for dispatchers when started
+ * TODO: implements beanpostprocessor
+ * */
+public class DispatcherPostBeanProcessor {
+}
